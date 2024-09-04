6e3dd13d77b17cdc286d3e93a5450c4ea4b075e2
Vũ Mạnh Hùng <hungxcbb@gmail.com>
2024-09-04 09:53:06 +0700

    up resolve conflct

--- demo/opengl_demo/main.cpp
+++ demo/opengl_demo/main.cpp
@@ -2920,7 +2920,7 @@ void DisplayFunc(void)
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
 
-	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClearColor(0.0, 0.0, 0.0, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glActiveTexture(GL_TEXTURE0);
 
@@ -3462,7 +3462,7 @@ int main(int argc, char* argv[])
 	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
 	glutInitWindowSize(CurrentWidth, CurrentHeight);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutCreateWindow("Blinn");
+	glutCreateWindow("A Living Room");
 
 	glewExperimental = GL_TRUE;
 	glewInit();
